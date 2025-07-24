be7d08815631496044d98c648c4300de505a121d
aditya kumar <adiartfriendship@gmail.com>
2025-07-24 19:33:05 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 "use client";
 
 import Image from 'next/image';
+import Link from 'next/link';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { Icons } from '@/components/icons';
