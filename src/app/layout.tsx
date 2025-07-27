f478c5b9c9b1d8966a648f841a3b241217c88056
Mohit Kumar <mohitkumar7232901@gmail.com>
2025-07-27 07:53:16 +0000

    Caused by:     Syntax Error Import trace for requested module: ./src/app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Space+Grotesk:wght@400;500;600;700&display=swap" rel="stylesheet" />
       </head>
       <body className={cn('font-body antialiased')} suppressHydrationWarning>
@@ -41,3 +41,5 @@ export default function RootLayout({
       </body>
     </html>
   );
+
+}
